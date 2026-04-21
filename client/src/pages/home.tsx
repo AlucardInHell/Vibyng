2c74e2302492a57114c53290cba3a03d56737856
Ivano Stagliano <ivano.stagliano@gmail.com>
2026-04-22 01:12:04 +0200

    Refactor share dialog structure in home.tsx
    Removed redundant div wrapping share dialog.

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1605,9 +1605,8 @@ const openInternalShare = () => {
       ))}
      {!!shareOptionsPost && !!shareDialogStep && (
   <div
-    <div
-  className="fixed inset-0 z-[100] bg-black/80 flex items-center justify-center p-4"
->
+    className="fixed inset-0 z-[100] bg-black/80 flex items-center justify-center p-4"
+  >
     <div
       className="w-full max-w-sm rounded-lg border bg-background p-6 shadow-lg"
       onPointerDown={(e) => e.stopPropagation()}
